17df324004ccedc9b126d0a1bd9df9cec865fd47
László Szécsi <Lszl Szcsi>
2016-06-30 18:57:30 +0200

    removing old trash

--- GearsPy/Project/Calibration/3_Fullfields/2_Dynamic/2_linear_increasing_@frame4651_measurement.py
+++
@@ -1,6 +0,0 @@
-#Calibration at 2016-06-29_10-15-06
-def apply(experiment) :
-    stim = experiment.getStimulusAtFrame(4651)
-
-    stim.setMeasuredDynamics( 0.03125, 1.00390625, 0.5260836482048035, 0.2957560420036316)
-                
