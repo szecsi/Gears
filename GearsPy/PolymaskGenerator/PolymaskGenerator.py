6c4916a69a4bc55bd63a9f6c45e415c7b9e00cbc
Kinyik Bence <kinyik94@gmail.com>
2018-10-02 00:19:42 +0200

    Move control points

--- GearsPy/PolymaskGenerator/PolymaskGenerator.py
+++ GearsPy/PolymaskGenerator/PolymaskGenerator.py
@@ -14,14 +14,11 @@ CATMULL_ROM_MTX = np.array([
             [ -0.5,  1.5, -1.5,  0.5]
         ])
 
-_catmullShader = None
-_vertexArray = None
-_vertexBuffer = None
-
 class PolymaskGenerator(QGLWidget):
     def __init__(self, parent, winId):
         super().__init__()
 
+        self.selectedControlIndex = None
         self.controlPoints = [
             np.array([0, 0]),
             np.array([0, 0.5]),
@@ -40,8 +37,30 @@ class PolymaskGenerator(QGLWidget):
         glViewport(0, 0, w, h)
 
     def paintGL(self):
+        glClear( GL_COLOR_BUFFER_BIT )
         self.vertecies = []
         self.drawCurve()
+
+    def mousePressEvent(self, e):
+        mouse = [(e.x()-self.width / 2) * 2, (-e.y()+self.height/2)*2]
+
+        self.selectedControlIndex = None
+        for idx, point in enumerate(self.controlPoints):
+            p = point.tolist()
+            if np.linalg.norm(np.array([p[0]*self.width, p[1]*self.height])-np.array(mouse)) < 10:
+                self.selectedControlIndex = idx
+
+    def mouseReleaseEvent(self, e):
+        self.selectedControlIndex = None
+
+    def mouseMoveEvent(self, e):
+        if self.selectedControlIndex == None:
+            return
+        mouse = [e.x()/self.width, (self.height-e.y())/self.height]
+        mouse = [(i-0.5)*2 for i in mouse]
+
+        self.controlPoints[self.selectedControlIndex] = np.array(mouse)
+        self.update()
 
     def drawCurve(self):
         for idx, val in enumerate(self.controlPoints):
@@ -59,14 +78,15 @@ class PolymaskGenerator(QGLWidget):
                 val,
                 self.controlPoints[i2],
                 self.controlPoints[i3]
-            ]),1)
+            ]))
 
         glEnableClientState( GL_VERTEX_ARRAY )
         glVertexPointer( 2, GL_FLOAT, 0, self.vertecies )
+        glColor3f(0.0, 0.0, 0.0)
         glDrawArrays( GL_LINE_STRIP, 0, int(len(self.vertecies)/2) )
         glDisableClientState( GL_VERTEX_ARRAY )
 
-    def drawSegment(self, P, precision = 10):
+    def drawSegment(self, P, precision = 100):
         self.vertecies.extend(P[1].tolist())
         t = 0
         while t < 1:
