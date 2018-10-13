0f11ad20a1aceb6185c7a3a0d19b5ab2e7bad3cf
Kinyik Bence <kinyik94@gmail.com>
2018-10-13 13:50:44 +0200

    Generate triangle vertecies for simple polygon
    Works wrongly with own intersecting polygons

--- GearsPy/PolymaskGenerator/PolymaskGenerator.py
+++ GearsPy/PolymaskGenerator/PolymaskGenerator.py
@@ -2,10 +2,18 @@ from PyQt5.QtGui import (QColor)
 from PyQt5.QtOpenGL import (QGLWidget)
 import GearsUtils as utils
 import numpy as np
+from Gears import CDT
+from enum import Enum
 try:
     from OpenGL.GL import *
 except:
     print ('ERROR: PyOpenGL not installed properly.')
+
+class PolymaskChangeEvent(Enum):
+    SelectionChanged = 0
+    ItemChanged = 1
+    ItemAdded = 2
+    ItemRemoved = 3
 
 CATMULL_ROM_MTX = np.array([
             [    0,    1,    0,    0],
@@ -15,40 +23,50 @@ CATMULL_ROM_MTX = np.array([
         ])
 
 class PolymaskGenerator(QGLWidget):
-    def __init__(self, parent, winId):
+    def __init__(self, parent, winId, controlPoints, dataChanged):
         super().__init__()
 
         self.selectedControlIndex = None
-        self.controlPoints = [
-            np.array([0, 0]),
-            np.array([0, 0.5]),
-            np.array([0.5, 0.5]),
-            np.array([0.5, 0])
-        ]
+        self.dataChanged = dataChanged
+        self.controlPoints = [np.array(a) for a in controlPoints]
 
         utils.initQGLWidget(self, super(), parent, winId)
 
-        color = QColor (0, 255, 0, 255)
+        color = QColor (255, 255, 255, 255)
         self.qglClearColor(color)
 
+        self.fill = None
+
     def resizeGL(self, w, h):
-        self.width = w
-        self.height = h
-        glViewport(0, 0, w, h)
+        self.setFixedSize(1280, 720)
+        glViewport(0, 0, 1280, 720)
 
     def paintGL(self):
         glClear( GL_COLOR_BUFFER_BIT )
-        self.vertecies = []
+        glEnableClientState( GL_VERTEX_ARRAY )
         self.drawCurve()
+        self.drawControlPoints()
+        glDisableClientState( GL_VERTEX_ARRAY )
+
+    def drawControlPoints(self):
+        cp_list = [ c for vec in [i.tolist() for i in self.controlPoints] for c in vec ]
+        glVertexPointer( 2, GL_FLOAT, 0, cp_list)
+        glColor3f(1.0, 0.0, 0.0)
+        glPointSize(10)
+        glDrawArrays( GL_POINTS, 0, int(len(self.controlPoints)) )
 
     def mousePressEvent(self, e):
-        mouse = [(e.x()-self.width / 2) * 2, (-e.y()+self.height/2)*2]
+        mouse = [(e.x()-self.width() / 2) * 2, (-e.y()+self.height()/2)*2]
 
         self.selectedControlIndex = None
         for idx, point in enumerate(self.controlPoints):
             p = point.tolist()
-            if np.linalg.norm(np.array([p[0]*self.width, p[1]*self.height])-np.array(mouse)) < 10:
+            if np.linalg.norm(np.array([p[0]*self.width(), p[1]*self.height()])-np.array(mouse)) < 10:
                 self.selectedControlIndex = idx
+                break
+
+        if self.selectedControlIndex != None:
+            self.onDataChanged(PolymaskChangeEvent.SelectionChanged, self.selectedControlIndex)
 
     def mouseReleaseEvent(self, e):
         self.selectedControlIndex = None
@@ -56,23 +74,26 @@ class PolymaskGenerator(QGLWidget):
     def mouseMoveEvent(self, e):
         if self.selectedControlIndex == None:
             return
-        mouse = [e.x()/self.width, (self.height-e.y())/self.height]
+        mouse = [e.x()/self.width(), (self.height()-e.y())/self.height()]
         mouse = [(i-0.5)*2 for i in mouse]
 
-        self.controlPoints[self.selectedControlIndex] = np.array(mouse)
-        self.update()
+        if not next((True for item in self.controlPoints if np.array_equal(item, np.array(mouse))), False):
+            self.controlPoints[self.selectedControlIndex] = np.array(mouse)
+            self.onDataChanged(PolymaskChangeEvent.ItemChanged, self.selectedControlIndex, mouse)
+            self.update()
 
     def drawCurve(self):
+        self.vertecies = []
         for idx, val in enumerate(self.controlPoints):
             i0 = idx - 1
             i2 = idx + 1
-            i3 = idx + 2 
+            i3 = idx + 2
             if i2 >= len(self.controlPoints):
                 i2 = 0
                 i3 = 1
             elif i3 >= len(self.controlPoints):
                 i3 = 0
-            
+
             self.drawSegment(np.array([
                 self.controlPoints[i0],
                 val,
@@ -80,24 +101,45 @@ class PolymaskGenerator(QGLWidget):
                 self.controlPoints[i3]
             ]))
 
-        glEnableClientState( GL_VERTEX_ARRAY )
+        if self.fill:
+            glVertexPointer( 2, GL_FLOAT, 0, self.fill )
+            glColor3f(0.8, 0.8, 0.8)
+            glDrawArrays( GL_TRIANGLES, 0, int(len(self.fill)/2) )
+
         glVertexPointer( 2, GL_FLOAT, 0, self.vertecies )
         glColor3f(0.0, 0.0, 0.0)
         glDrawArrays( GL_LINE_STRIP, 0, int(len(self.vertecies)/2) )
-        glDisableClientState( GL_VERTEX_ARRAY )
 
-    def drawSegment(self, P, precision = 100):
+    def drawSegment(self, P, precision = 50):
         self.vertecies.extend(P[1].tolist())
-        t = 0
-        while t < 1:
-            t += 1 / precision
+        t = 1 / precision
+        while t < 0.99999999999999:
             catmull_rom_vec = np.matmul(np.array([
                 1, t, t*t, t*t*t
             ]), CATMULL_ROM_MTX)
 
             ft = np.dot(catmull_rom_vec, P)
             self.vertecies.extend([float(i) for i in ft.tolist()])
+            t += 1 / precision
 
-    def show(self, controlPoints):
-        self.points = controlPoints
-        super().show()
+    def addBefore(self, index):
+        if not next((True for item in self.controlPoints if np.array_equal(item, np.array([0,0]))), False):
+            self.controlPoints.insert(index, np.array([0,0]))
+            self.onDataChanged(PolymaskChangeEvent.ItemAdded, index, [0,0])
+            self.update()
+
+    def addAfter(self, index):
+        if not next((True for item in self.controlPoints if np.array_equal(item, np.array([0,0]))), False):
+            self.controlPoints.insert(index+1, np.array([0,0]))
+            self.onDataChanged(PolymaskChangeEvent.ItemAdded, index+1, [0,0])
+            self.update()
+
+    def generateTriangles(self):
+        cdt = CDT(self.vertecies)
+        cdt.triangulate()
+        self.fill = cdt.get_triangles()
+        self.update()
+
+    def onDataChanged(self, type, index, value = None):
+        self.fill = None
+        self.dataChanged(type, index, value)
--- GearsPy/PolymaskGenerator/PolymaskGeneratorWindow.py
+++ GearsPy/PolymaskGenerator/PolymaskGeneratorWindow.py
@@ -1,26 +1,81 @@
-from PyQt5.QtWidgets import (QWidget, QSplitter, QGridLayout, QPushButton)
-from PolymaskGenerator.PolymaskGenerator import *
+from PyQt5.QtWidgets import (QWidget, QSplitter, QGridLayout, QPushButton, QVBoxLayout, QListWidget)
+from PolymaskGenerator.PolymaskGenerator import PolymaskGenerator, PolymaskChangeEvent
 
 class PolymaskGeneratorWindow(QWidget):
-    def __init__(self):
+    def __init__(self, controlPoints = None):
         super().__init__()
-        self.setMinimumSize(1600, 720)
-        self.setMaximumSize(1600, 720)
-        self.setGeometry(100, 100, 1600, 720)
+        self.setMinimumSize(1628, 742) # 1600 plus margins
+        self.setMaximumSize(1628, 742)
+        self.setGeometry(100, 100, 1628, 742)
 
         layout = QGridLayout(self)
-
-        self.polymaskGenerator = PolymaskGenerator(self, self.winId())
-        layout.addWidget(self.polymaskGenerator, 1, 1, 1, 4)
-
-        right_panel = QWidget(None)
-        right_panel_layout = QGridLayout()
-
-        saveButton = QPushButton("Save", right_panel)
-        right_panel_layout.addWidget(saveButton, 1, 1, 1, 1)
-
-        right_panel.setLayout(right_panel_layout)
-        layout.addWidget(right_panel, 1 ,5, 1, 1)
-
         self.setLayout(layout)
         self.setWindowTitle("Polymask Generator")
+
+        if controlPoints == None:
+            controlPoints = [
+                [-0.25, -0.25],
+                [-0.25, 0.25],
+                [0.25, 0.25],
+                [0.25, -0.25]
+            ]
+
+        self.polymaskGenerator = PolymaskGenerator(self, self.winId(), controlPoints, self.dataChanged)
+        layout.addWidget(self.polymaskGenerator)
+        layout.setColumnStretch(0, 4)
+
+        right_panel = QWidget(self)
+        right_panel_layout = QVBoxLayout(right_panel)
+        right_panel.setLayout(right_panel_layout)
+
+        saveButton = QPushButton("Save", right_panel)
+        right_panel_layout.addWidget(saveButton)
+
+        generateButton = QPushButton("Show filled polygon", right_panel)
+        generateButton.clicked.connect(self.generateTriangles)
+        right_panel_layout.addWidget(generateButton)
+        
+        self.cp_list = QListWidget()
+
+        idx = 0
+        for cp in controlPoints:
+            if len(cp) < 2:
+                print("Error: controlPoint size less then 2")
+                continue
+            self.cp_list.addItem(self.pointToString(idx, cp))
+            idx += 1
+
+        self.cp_list.setFixedHeight(200)
+        self.cp_list.setCurrentRow(0)
+        right_panel_layout.addWidget(self.cp_list)
+
+        addBeforeButton = QPushButton("Add Controlpoint before selected", right_panel)
+        addBeforeButton.clicked.connect(self.addBefore)
+        right_panel_layout.addWidget(addBeforeButton)
+
+        addAfterButton = QPushButton("Add Controlpoint after selected", right_panel)
+        addAfterButton.clicked.connect(self.addAfter)
+        right_panel_layout.addWidget(addAfterButton)
+
+        layout.addWidget(right_panel, 0, 1)
+        layout.setColumnStretch(1, 1)
+
+    def pointToString(self, idx, point):
+        return "P" + str(idx) + " ( " + "{:.4f}".format(point[0]) + ", " + "{:.4f}".format(point[1]) + " )"
+
+    def addBefore(self):
+        self.polymaskGenerator.addBefore(self.cp_list.currentRow())
+
+    def addAfter(self):
+        self.polymaskGenerator.addAfter(self.cp_list.currentRow())
+
+    def generateTriangles(self):
+        self.polymaskGenerator.generateTriangles()
+
+    def dataChanged(self, type, index, value = None):
+        if type == PolymaskChangeEvent.SelectionChanged:
+            self.cp_list.setCurrentRow(index)
+        elif type == PolymaskChangeEvent.ItemChanged:
+            self.cp_list.item(index).setText(self.pointToString(index, value))
+        elif type == PolymaskChangeEvent.ItemAdded:
+            self.cp_list.insertItem(index, self.pointToString(index, value))
